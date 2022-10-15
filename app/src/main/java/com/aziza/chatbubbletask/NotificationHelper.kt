1cd13a7ce6bb4c3a4dc0fdc5c3e16abe62313775
Aziza Helmy <aziza.helmy35@gmail.com>
2022-10-15 14:20:16 +0200

    .

--- app/src/main/java/com/aziza/chatbubbletask/NotificationHelper.kt
+++ app/src/main/java/com/aziza/chatbubbletask/NotificationHelper.kt
@@ -122,28 +122,28 @@ class NotificationHelper(private val context: Context) {
 
         val builder = Notification.Builder(context, CHANNEL_NEW_MESSAGES)
             // TODO 5: Set up a BubbleMetadata.
-//            .setBubbleMetadata(
-//                Notification.BubbleMetadata
-//                    .Builder(
-//                        PendingIntent.getActivity(
-//                            context,
-//                            REQUEST_BUBBLE,
-//                            Intent(context, BubbleActivity::class.java)
-//                                .setAction(Intent.ACTION_VIEW)
-//                                .setData(contentUri),
-//                            PendingIntent.FLAG_UPDATE_CURRENT
-//                        ),
-//                        icon
-//                    )
-//                    .setDesiredHeightResId(R.dimen.bubble_height)
-//                    .apply {
-//                        if (fromUser) {
-//                            setAutoExpandBubble(true)
-//                            setSuppressNotification(true)
-//                        }
-//                    }
-//                    .build()
-//            )
+            .setBubbleMetadata(
+                Notification.BubbleMetadata
+                    .Builder(
+                        PendingIntent.getActivity(
+                            context,
+                            REQUEST_BUBBLE,
+                            Intent(context, BubbleActivity::class.java)
+                                .setAction(Intent.ACTION_VIEW)
+                                .setData(contentUri),
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                        ),
+                        icon
+                    )
+                    .setDesiredHeightResId(R.dimen.bubble_height)
+                    .apply {
+                        if (fromUser) {
+                            setAutoExpandBubble(true)
+                            setSuppressNotification(true)
+                        }
+                    }
+                    .build()
+            )
             // The user can turn off the bubble in system settings. In that case, this notification
             // is shown as a normal notification instead of a bubble. Make sure that this
             // notification works as a normal notification as well.
