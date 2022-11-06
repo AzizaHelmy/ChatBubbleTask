8a833508655da48ce90bdb86adacb903b72a5e3a
Aziza Helmy <aziza.helmy35@gmail.com>
2022-11-06 11:27:02 +0200

    adding bubble

--- app/src/main/java/com/aziza/chatbubbletask/MainActivity.kt
+++ app/src/main/java/com/aziza/chatbubbletask/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.aziza.chatbubbletask
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -14,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private  val CHANNEL_ID = "1"
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,6 +32,12 @@ class MainActivity : AppCompatActivity() {
                 .setContentTitle("textTitle")
                 .setContentText("textContent")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setBubbleMetadata(
+                    Notification.BubbleMetadata
+                        .Builder()
+                        .setDesiredHeightResId(R.dimen.bubble_height)
+                        .build()
+                )
 
             with(NotificationManagerCompat.from(this)) {
                 notify(1, builder.build())
