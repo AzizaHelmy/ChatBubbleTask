bce5c94d8470b4e3a5079e40f58402271be3d578
Aziza Helmy <aziza.helmy35@gmail.com>
2022-11-06 11:13:44 +0200

    notification appears

--- app/src/main/java/com/aziza/chatbubbletask/MainActivity.kt
+++ app/src/main/java/com/aziza/chatbubbletask/MainActivity.kt
@@ -1,91 +1,52 @@
 package com.aziza.chatbubbletask
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.aziza.chatbubbletask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val NOTIFICATION_ID = 77
-
-
+    private  val CHANNEL_ID = "1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationHelper.setUpNotificationChannels(this)
+        createNotificationChannel()
+
+        binding.send.setOnClickListener {
+            var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_message)
+                .setContentTitle("textTitle")
+                .setContentText("textContent")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(1, builder.build())
+            }
         }
 
 
-        binding.send.setOnClickListener {
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                NotificationHelper.showNotification(this,R.string.channel_new_messages,R.string.channel_new_messages_description,NOTIFICATION_ID)
+    }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.description_send)
+            val descriptionText = getString(R.string.description_send)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
             }
-//           // val icon = Icon.createWithAdaptiveBitmapContentUri("cat.jpg")
-////            val user = android.app.Person.Builder().setName(context.getString(R.string.sender_you)).build()
-////            val person = android.app.Person.Builder().setName(chat.contact.name).setIcon(icon).build()
-//            val contentUri = "https://android.example.com/chat/${1}".toUri()
-//
-//            // Create bubble intent
-//            val target = Intent(this, BubbleActivity::class.java)
-//            val bubbleIntent = PendingIntent.getActivity(this, 0, target, 0 /* flags */)
-//
-//            // Create notification
-//            val chatBot = Person.Builder()
-//                .setBot(true)
-//                .setName("BubbleBot")
-//                .setImportant(true)
-//                .build()
-//             // Create bubble metadata
-//            val bubbleData = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                Notification.BubbleMetadata.Builder()
-//                    .setDesiredHeight(600)
-//                    // Note: although you can set the icon is not displayed in Q Beta 2
-//                    .setIcon(Icon.createWithResource(this, R.drawable.remove))
-//                    .setIntent(bubbleIntent)
-//                    .build()
-//            } else {
-//                Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
-//
-//            }
-//
-//            val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                Notification.Builder(this, CHANNEL_ID)
-//                    //.setContentIntent(contentIntent)
-//                    .setSmallIcon(R.drawable.remove)
-//                    .setBubbleMetadata(
-//                        NotificationCompat.Builder( PendingIntent.getActivity(
-//                            this,
-//                            REQUEST_BUBBLE,
-//                            Intent(this, BubbleActivity::class.java)
-//                                .setAction(Intent.ACTION_VIEW)
-//                                .setData(contentUri),
-//                            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-//                        ),
-//                            null)
-//                       .setDesiredHeight(600)
-//                        // Note: although you can set the icon is not displayed in Q Beta 2
-//                        .setIcon(Icon.createWithResource(this, R.drawable.remove))
-//                        .setIntent(bubbleIntent)
-//                        .build())
-//                    .addPerson( android.app.Person.Builder()
-//                        .setName("contact.name")
-//                        //.setIcon("icon")
-//                        .build())
-//            } else {
-//                TODO("VERSION.SDK_INT < Q")
-//            }
-//            builder.build()
-//
-//        }
-//
-//    }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
